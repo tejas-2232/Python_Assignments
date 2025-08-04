2d3f7c21de13f67cdc6c75d1364b6e86e9b3f991
tejas-2232 <tejasbachhav98@gmail.com>
2025-08-03 23:54:21 -0400

    add q7.py

---
+++ beginner/level3/q7.py
@@ -0,0 +1,17 @@
+students = ["Sam", "Alice", "Mona"]
+subjects = ["Commerce", "Science", "Computer"]
+
+students_subjects = {}
+
+for i in range(len(students)):
+    student = students[i]
+    subject = subjects[i]
+    students_subjects[student] = subject
+
+print(students_subjects)
+
+
+
+
+
+
